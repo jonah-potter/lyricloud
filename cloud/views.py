636148b3381108826dc28b1e24000e2c380c92e2
Jonah <jonah@ajpotter.com>
2018-04-15 22:24:17 -0400

    refined models, added seed data for a song

--- cloud/views.py
+++ cloud/views.py
@@ -7,10 +7,19 @@ def index(request):
     return HttpResponse("Hello, World!")
 
 def song(request):
-    s = models.Song(title='Find Your Cloud', credit='Papadosio')
+    # i know this is gross but it's dummy data
+    s = models.Song(title='Find Your Cloud',
+                    credit='Papadosio',
+                    lyrics="Dream out loud and make no sound\nFind your cloud and ride it 'round")
     s.save()
-    w = models.Word(text='dream')
-    w.save()
-    sw = models.SongWord.objects.create(song=s, word=w, count=1)
-    response = s.title+" by "+s.credit+" contains the word(s) \""+w.text+"\" "+str(sw.count)+" time(s)."
-    return HttpResponse(response)
+    wordStrings = s.lyrics.replace('\n', ' ').split(' ')
+    for wordString in wordStrings:
+        wordString = wordString.lower()
+        # get_or_create returns a tuple with the object gotten or created and a boolean value reflecting whether it was created
+        w = models.Word.objects.get_or_create(text=wordString)[0]
+        # normally you could just use s.words.add(w),
+        # but Song and Word relate through an intermediate join table with fields of its own
+        sw = models.SongWord.objects.create(song=s, word=w, count=wordStrings.count(wordString))
+        w.save()
+        sw.save()
+    return HttpResponse(s.title+" by "+s.credit+" contains the word(s) \""+w.text+"\" "+str(sw.count)+" time(s).")
